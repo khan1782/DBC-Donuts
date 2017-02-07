593983ab5c56df2a16cb1d16f3490d0c6cd46ff5
kevin <khan1782@gmail.com>
2017-02-06 22:40:11 -0600

    fixed rounding, added drop down instead of form field

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,20 @@
-get '/orders' do
+get '/orders/new' do
 	@current_donutshop = Donutshop.find_by_current_shop("true")
 	erb :'orders/view'
+end
+
+
+post '/orders' do 
+
+	# @current_donutshop = Donutshop.find_by_current_shop("true")
+	# current_wholesale = Wholesale.last
+	# current_order 		=	Order.new(user_id: session[:user_id], wholesale_id: current_wholesale.id)
+
+	# if current_order.save
+	# 	params["donut"].each {|donut| current_order.add(donut)}
+		redirect '/'
+	# else
+	# 	@errors = current_order.errors.full_messages
+	# 	erb :'menuitems/index'
+	# end
 end
--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -1,3 +1,4 @@
 def money(float)
-	float.round(2)
+	rounded_string = float.round(2).to_s
+	rounded_string[-1] == "0" ? rounded_string + "0" : rounded_string
 end
