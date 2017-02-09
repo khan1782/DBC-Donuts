5cb660725a71fcc49442761d9254dcf53d3673c3
kevin <khan1782@gmail.com>
2017-02-08 23:31:42 -0600

    making majhor changfes

--- public/js/application.js
+++ public/js/application.js
@@ -1,66 +1,43 @@
 $(document).ready(function() {
 
-// //on register button 'click' present form
-// $(".login-nav").on("click","a#register", function(event){
-// 	event.preventDefault();
-//   console.log("clicked")
-//   $.ajax({
-//     url: '/users/new',
-//     type: "GET"
-//   }).done(function(response){
-//     //get back an erb string and append to the login-nav class
-//       $(".login-nav").append(response);
-//     });
-// });
+
+//login button
+$("#nav-section").on("click",".login", function(event){
+  $("#login-section").slideToggle();
+});
+
+//register button
+$("#nav-section").on("click",".register", function(event){
+  $("#registration-section").slideToggle();
+});
+
+//menu button
+$("#nav-section").on("click",".menu", function(event) {
+  $("#menu-section").slideToggle();
+})
+
+//order button
+$("#nav-section").on("click",".order", function(event) {
+  $("#order-section").slideToggle();
+});
 
 
-// on submit of registration form send post request and remove form
-// $("section.login-nav").on("click","form.new-user-form input[type=submit]", function(event){
-// 	event.preventDefault();
-//   //grab data from registration form that was just sent out
-// 	var data = $(".new-user-form").serialize();
-//   console.log("clicked the registration form")
-//   //set up ajax POST for creating new user
-// 	$.ajax({
-// 		url: '/users',
-// 		type: "POST",
-// 		data: data
-// 	}).done(function(response){
-//     $("section.login-nav").append(response);
-//     $("a#login").hide();
-//     $("span.index-links").append("<a id='order-button' href='/orders/new'>Order now</a>");
-//   });
-// 	$(".registration").remove();
-// });
-
-// //on click of logout button that was added after registration
-// $("section.login-nav").on("click","a#logout", function(event){
-//   event.preventDefault()
-//   $.ajax({
-//     url: '/sessions',
-//     type: "DELETE"
-//   })
-//   $("section.dynamic-login-nav").remove()
-//   /////////////////////////////////////////////////////////////////
-// });
-
-//on click of login button
-$("div.nav-section").on("click","a.login", function(event){
-  event.preventDefault();
-  $("div.login-section").slideDown()
+//admin-button
+$("#nav-section").on("click",".admin", function(event) {
+  $("#admin-section").slideToggle();
 });
 
 
 //on click of submit of login form send post request and log person in
-$("div.login-section").on("click", "input.login-button", function(event) {
+$("#login-section").on("submit", "form.login-form", function(event) {
   event.preventDefault()
   var data = $("form.login-form").serialize();
   $.ajax({
-  	url: '/sessions',
-  	type: 'POST',
-  	data: data
+    url: '/sessions',
+    type: 'POST',
+    data: data
   }).done(function(response){
-    $("div.login-section").slideUp()
+    $("#login-section").slideUp()
     $("div.nav-section").children("a.profile").show()
     $("div.nav-section").children("a.logout").show()
     $("div.nav-section").children("a.register").css("display","none")
@@ -69,19 +46,11 @@ $("div.login-section").on("click", "input.login-button", function(event) {
   }).fail(function(error){
     //put what happens when they are not authorized
   });
-
-
-});
-
-//click on the order button on home page
-$("div.banner-section").on("click", "a#order-button", function(event){
-  event.preventDefault();
-  $("div.order-section").slideDown();
 });
 
 
 // click on the order row for donut order form
-$("div.order-row").on("click", "select", function(event){
+$("#order-section").on("click", ".drop-down", function(event){
   event.preventDefault;
   var data = $(this).serialize()
   $.ajax({
@@ -90,22 +59,22 @@ $("div.order-row").on("click", "select", function(event){
     data: data
   }).done(function(response){
     var orderDetails =JSON.parse(response)
-    $("label.subtotal").html("subtotal: $" + orderDetails.subtotal)
-    $("label.delivery").html("delivery: $" + orderDetails.delivery_cost)
-    $("label.tax").html("tax: $" + orderDetails.tax)
-    $("label.total").html("total: $" + orderDetails.total)
+    $("label#subtotal").html("subtotal: $" + orderDetails.subtotal)
+    $("label#delivery").html("delivery: $" + orderDetails.delivery_cost)
+    $("label#tax").html("tax: $" + orderDetails.tax)
+    $("label#total").html("total: $" + orderDetails.total)
     $("div.order-section").find("input.id").val(orderDetails.id)
   });
 });
 
 
 
+
 //click on the submit button of the order form 
-$(".order-section").on("click","#submit-unconfirmed",function(event){
+$("#order-section").on("submit","form.order-form",function(event){
   event.preventDefault()
-  console.log("AD")
-  $("div.order-section").slideUp()
-  $("div.confirmation-section").slideDown();
+  $("#order-section").slideUp()
+  $("#confirmation-section").slideDown();
   // var data = $("form#order-form").serialize() 
   var id = $(this).siblings("input.id").val()
   $.ajax({
@@ -113,14 +82,53 @@ $(".order-section").on("click","#submit-unconfirmed",function(event){
     type:"GET",
   }).done(function(response){
     var orderDetails =JSON.parse(response)
-    $("div.confirmation-section").find("li.delivery-cost").append(orderDetails.delivery_cost);
-    $("div.confirmation-section").find("li.subtotal").append(orderDetails.subtotal);
-    $("div.confirmation-section").find("li.tax").append(orderDetails.tax);
-    $("div.confirmation-section").find(".total").append(orderDetails.total);
-    $("div.confirmation-section").find(".order-maker").append(orderDetails.user + "'s Order");
-    $("div.confirmation-section").find(".deadline").append(orderDetails.deadline);
+    console.log(orderDetails)
+    $("#confirmation-section").find("li.delivery-cost").append(orderDetails.delivery_cost);
+    $("#confirmation-section").find("li.subtotal").append(orderDetails.subtotal);
+    $("#confirmation-section").find("li.tax").append(orderDetails.tax);
+    $("#confirmation-section").find(".total").append(orderDetails.total);
+    $("#confirmation-section").find(".order-maker").append(orderDetails.user + "'s Order");
+    $("#confirmation-section").find(".deadline").append(orderDetails.deadline);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //on click of confirmation submit button
 $("div.confirmation-section").on("click","input.order-confirmation", function(event){
@@ -136,16 +144,6 @@ $("div.confirmed-section").on("click", function(event){
 })
 
 
+
+
 });
-
-var Order = function(id){
-  this.id = id
-  this.subtotal = null
-  this.delivery_cost = null
-  this.tax = null
-  this.total = null
-}
-
-
-
-// });
